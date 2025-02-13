c09199c52ec11c2be9799c0c13286a809f6fb191
Marilise Morona <marymorona@gmail.com>
2025-02-13 20:29:30 -0300

    adjust font and spacing in texts

--- src/modules/Body/Skills.js
+++ src/modules/Body/Skills.js
@@ -2,7 +2,6 @@ import './skills.css';
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import skillsBg from '../../assets/img/banner_home.png';
 import agritech from '../../assets/img/agritech_lineWhite.png';
 import engineer from '../../assets/img/engineerSoftware_lineWhite.png';
 import business from '../../assets/img/business_lineWhite.png';
@@ -32,32 +31,34 @@ export const Skills = () => {
             <Container>
                 <Row>
                     <Col>
-                    <div className="skill-bx">
-                        <h2>Formações</h2>
-                        <p> Formada em MBA em Engenharia de Software pela UTFPR e MBA em Digital Business pela ESALQ/USP, desenvolvi uma visão ampla sobre a transformação digital em uma sociedade cada vez mais tecnológica. Acredito que, mais do que simplesmente criar produtos tecnológicos, é essencial construir soluções que inspirem confiança, garantam segurança, promovam eficiência e impulsionem a sustentabilidade.</p>
-                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                            <div className="item">
-                                <img src={agritech} alt="Image"/>
-                                <h5>Agrônoma</h5>
-                            </div>
-                            <div className="item">
-                                <img src={engineer} alt="Image"/>
-                                <h5>Engenheira de Software</h5>
-                            </div>
-                            <div className="item">
-                                <img src={business} alt="Image"/>
-                                <h5>Especialista em Negócios Digitais</h5>
-                            </div>
-                            <div className="item">
-                                <img src={esg} alt="Image"/>
-                                <h5>Especialista em Economia e Gestão Ambiental</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+                        <div className="skill-bx">
+                            <h2>Formações</h2>
+                            <p> Formada em MBA em Engenharia de Software pela UTFPR e MBA em Digital Business pela ESALQ/USP, 
+                                desenvolvi uma visão ampla sobre a transformação digital em uma sociedade cada vez mais tecnológica. 
+                                Acredito que, mais do que simplesmente criar produtos tecnológicos, é essencial construir soluções 
+                                que inspirem confiança, garantam segurança, promovam eficiência e impulsionem a sustentabilidade.</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={agritech} alt="Image"/>
+                                    <h5>Agrônoma</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={engineer} alt="Image"/>
+                                    <h5>Engenheira de Software</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={business} alt="Image"/>
+                                    <h5>Especialista em Negócios Digitais</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={esg} alt="Image"/>
+                                    <h5>Especialista em Economia e Gestão Ambiental</h5>
+                                </div>
+                            </Carousel>
+                        </div>
                     </Col>
                 </Row>
             </Container>
-            {/*<img className="skillsBg" src={skillsBg} alt="Image" />*/}
         </section>
     )
 }
