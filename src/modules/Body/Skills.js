5b0e6e7d891bd0f02dc6b89b91dd4e2009d7d594
Marilise Morona <marymorona@gmail.com>
2025-06-03 14:33:03 -0300

    edit layout skills

--- src/modules/Body/Skills.js
+++ src/modules/Body/Skills.js
@@ -6,6 +6,7 @@ import agritech from '../../assets/img/agritech_lineWhite.png';
 import engineer from '../../assets/img/engineerSoftware_lineWhite.png';
 import business from '../../assets/img/business_lineWhite.png';
 import esg from '../../assets/img/esg_lineWhite.png';
+import amb from '../../assets/img/managerAmb_lineWhite.png';
 
 export const Skills = () => {
     const responsive = {
@@ -35,7 +36,7 @@ export const Skills = () => {
                             <h2>Formações</h2>
                             <p> Formada em MBA em Engenharia de Software pela UTFPR e MBA em Digital Business pela ESALQ/USP, 
                                 desenvolvi uma visão ampla sobre a transformação digital em uma sociedade cada vez mais tecnológica. 
-                                Acredito que, mais do que simplesmente criar produtos tecnológicos, é essencial construir soluções 
+                                Acredito que, mais do que simplesmente criar novos produtos, é essencial construir soluções 
                                 que inspirem confiança, garantam segurança, promovam eficiência e impulsionem a sustentabilidade.</p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
@@ -52,7 +53,11 @@ export const Skills = () => {
                                 </div>
                                 <div className="item">
                                     <img src={esg} alt="Image"/>
-                                    <h5>Especialista em Economia e Gestão Ambiental</h5>
+                                    <h5>Especialista em Economia Ambiental - ESG</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={amb} alt="Image"/>
+                                    <h5>Gestora Ambiental</h5>
                                 </div>
                             </Carousel>
                         </div>
