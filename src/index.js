b328ec4a73a84e6674f08317110bfa56b5fef0d0
MARILISE MORONA <marilise.morona@madeinweb.app>
2026-05-07 16:44:31 -0300

    resolved error deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './App';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 reportWebVitals();
